1090201fb77068a5e39653727f2c4f97774142b6
Ruiz Julián <ruizjulian710@gmail.com>
2023-07-15 16:38:07 -0300

    actualizacion

---
+++ javascript.js
@@ -0,0 +1,116 @@
+
+
+// function autenticarUsuario(user, password){
+//     if((user=='julianr')&&(password=='123')){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// let usuario = prompt('Cual es tu nombre de usuario?');
+// let contrasenia = prompt('Cual es tu contraseña?');
+
+// let autenticado = autenticarUsuario(usuario, contrasenia);
+// console.log(autenticado);
+
+// if(autenticado){
+//     alert("¡Bienvenido a RzShop!",usuario);
+// }else{
+//     alert('Usuario no reconocido, vuelve a intentarlo.');
+// }
+
+
+// //Calcular interés de pago con tarjeta de crédito
+
+
+// function calcularInt(precio){
+//     let Int = precio * 0.30;
+//     console.log('El interés de $'+precio+' es $'+Int);
+// }
+
+// calcularInt(100);
+
+// let valorProducto = parseFloat(prompt('Ingresa el precio del producto'));
+// calcularInt(valorProducto);
+
+// let precioi12 = 300000;
+// let costEnvio = 2000;
+
+// let suma = precioi12 + costEnvio;
+
+// let resta = precioi12 - costEnvio;
+
+// let multip = precioi12 * costEnvio; 
+
+// let divide = precioi12 / costEnvio; 
+
+
+
+const btnCart = document.querySelector (".shop-rz")
+const containerCartProducts = document.querySelector("article-shop")
+
+btnCart.addEventListener("click", () =>{
+    containerCartProducts.classList.toggle(hidden-cart)
+}) 
+
+
+const cartInfo = documente.querySelector(".article-shop")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
